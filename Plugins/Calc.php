c96c8318e5d27ed6e85f361c4cc4000e5deb48a3
Mosi Dev <mosi_dev@yahoo.com>
2017-01-31 00:20:13 +0330

    Create Calc.php

---
+++ Plugins/Calc.php
@@ -0,0 +1,13 @@
+<?php
+include "../Access.php";
+include "../index.php";
+/* The Source Bot: @NoMoBot
+Channel News: @Norbert_Team
+Support: @MosiDevBot
+*/
+if(preg_match('/^\/([Cc]alc) (.*)/s',$text)){
+    preg_match('/^\/([Cc]alc) (.*)/s',$text,$match);
+    $rs = file_get_contents('http://api.mathjs.org/v1/?expr='.urlencode($match[2]));
+	SendMessage($chat_id , "<code>".$rs."</code>" , 'html');
+  }
+  ?>
