85a3d5ad934f3d8fd8cf6719a9c203db007b6bb8
Mosi Dev <mosi_dev@yahoo.com>
2017-02-06 20:32:28 +0330

    Update Date-Time.php

--- Plugins/Date-Time.php
+++ Plugins/Date-Time.php
@@ -9,14 +9,14 @@ $date_and_time = json_decode(file_get_contents('http://api.norbert-team.ir/date-
 if(preg_match('/^\/([Dd]ate)/s',$text)){
 	$date_fa = $date_and_time->date_fa;
 	$date_en = $date_and_time->date_en;
-SendAction($chat_id,'typing');
+	SendAction($chat_id,'typing');
 	SendMessage($chat_id , "<b>تاریخ فارسی:</b> $date_fa
 	<b>تاریخ انگلیسی:</b> $date_en","html");
   }
   elseif(preg_match('/^\/([Tt]ime)/s',$text)){
 	$time_fa = $date_and_time->time_fa;
 	$time_en = $date_and_time->time_en;
-SendAction($chat_id,'typing');
+	SendAction($chat_id,'typing');
 	SendMessage($chat_id , "<b>ساعت فارسی:</b> $time_fa
 	<b>ساعت انگلیسی:</b> $time_en","html");
   }
