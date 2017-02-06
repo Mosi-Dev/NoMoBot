ca565873719ab93b3b0e93e639da56f18d42902e
Mosi Dev <mosi_dev@yahoo.com>
2017-02-06 20:39:00 +0330

    Update Start-Help.php

--- Plugins/Start-Help.php
+++ Plugins/Start-Help.php
@@ -7,9 +7,11 @@ Support: @MosiDevBot
 */
 
 if(preg_match("/^\/([Ss]tart)/",$text)){
+	SendAction($chat_id,'typing');
 	SendMessage($chat_id , $start , "html");
 }
 elseif(preg_match("/^\/([Hh]elp)/",$text)){
+	SendAction($chat_id,'typing');
 	SendMessage($chat_id , $help , "html");
 }
 
