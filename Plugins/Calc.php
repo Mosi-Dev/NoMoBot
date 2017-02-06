728f81b9e81b3ae13976aad5f011294ba2933ccb
Mosi Dev <mosi_dev@yahoo.com>
2017-02-06 20:30:49 +0330

    Update Calc.php

--- Plugins/Calc.php
+++ Plugins/Calc.php
@@ -7,7 +7,7 @@ Support: @MosiDevBot
 */
 if(preg_match('/^\/([Cc]alc) (.*)/s',$text)){
     preg_match('/^\/([Cc]alc) (.*)/s',$text,$match);
-SendAction($chat_id,'typing');
+    SendAction($chat_id,'typing');
     $rs = file_get_contents('http://api.mathjs.org/v1/?expr='.urlencode($match[2]));
 	SendMessage($chat_id , "<code>".$rs."</code>" , 'html');
   }
